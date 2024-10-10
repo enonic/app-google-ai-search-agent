d592ce242284ce0f6076617cbe2cbe6e0be01262
Pavel Milkevich <pmi@enonic.com>
2024-10-10 16:12:46 +0200

    Support default JSON filename/location #11

--- src/main/java/com/enonic/app/googlesearch/GoogleAuth.java
+++ src/main/java/com/enonic/app/googlesearch/GoogleAuth.java
@@ -11,12 +11,15 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.enonic.xp.app.Application;
 import com.enonic.xp.app.ApplicationKey;
 import com.enonic.xp.app.ApplicationService;
+import com.enonic.xp.home.HomeDir;
 import com.enonic.xp.script.bean.BeanContext;
 import com.enonic.xp.script.bean.ScriptBean;
 
 public class GoogleAuth
     implements ScriptBean
 {
+    public static String DEFAULT_JSON_PATH = "${xp.home}/config/google-service-account.json";
+
     private static final Logger LOG = LoggerFactory.getLogger( GoogleAuth.class );
 
     private ApplicationService appService;
@@ -25,23 +28,25 @@ public class GoogleAuth
     {
         try
         {
-            Application app = this.appService.get( ApplicationKey.from( getClass()) );
-            String path = app.getConfig().get( "google.serviceAccountJson" );
-            LOG.debug( "App config 'google.serviceAccountJson': {}", path );
+            Application app = this.appService.get( ApplicationKey.from( getClass() ) );
+            String path = app.getConfig()
+                .getOrDefault( "google.serviceAccountJson", DEFAULT_JSON_PATH.replace( "${xp.home}", HomeDir.get().toString() ) );
 
-            if ( path != null )
+            LOG.debug( "Using google service account file: {}", path );
+
+            File jsonFile = new File( path );
+            if ( jsonFile.exists() )
             {
-                File jsonFile = new File( path );
-                LOG.debug( "Service account json {} exists: {}", path, jsonFile.exists() );
-                if ( jsonFile.exists() )
-                {
-                    GoogleCredentials credentials = GoogleCredentials.fromStream( jsonFile.toURI().toURL().openStream() ).createScoped(
-                        "https://www.googleapis.com/auth/cloud-platform" );
+                GoogleCredentials credentials = GoogleCredentials.fromStream( jsonFile.toURI().toURL().openStream() )
+                    .createScoped( "https://www.googleapis.com/auth/cloud-platform" );
 
-                    credentials.refreshIfExpired();
+                credentials.refreshIfExpired();
 
-                    return new AccessTokenMapper( credentials.getAccessToken() );
-                }
+                return new AccessTokenMapper( credentials.getAccessToken() );
+            }
+            else
+            {
+                LOG.warn( "Google service account file does not exist: {}", path );
             }
 
             return null;
